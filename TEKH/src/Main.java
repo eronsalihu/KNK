1a62087ea3d276fc81f24783f46c311e09e7c19e
Enis Berisha <58141985+enisbe1@users.noreply.github.com>
2020-06-27 09:42:24 +0200

    Update Main.java

--- TEKH/src/Main.java
+++ TEKH/src/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("/Views/Intro.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/Views/SignIn.fxml"));
 
         Image applicationIcon = new Image(getClass().getResourceAsStream("/Icons/icon.png"));
         primaryStage.getIcons().add(applicationIcon);
