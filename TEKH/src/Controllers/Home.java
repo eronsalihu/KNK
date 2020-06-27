df01bafeb9a478fe322c8b9d2a3a874eea49369a
Enis Berisha <58141985+enisbe1@users.noreply.github.com>
2020-06-27 14:23:13 +0200

    Update Home.java

--- TEKH/src/Controllers/Home.java
+++ TEKH/src/Controllers/Home.java
@@ -1,17 +1,18 @@
 package Controllers;
 
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
+import javafx.animation.*;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -46,6 +47,26 @@ public class Home implements Initializable {
     @FXML
     private Label email;
 
+    @FXML
+    private StackPane stackPane;
+
+    @FXML
+    private Pane rightPane;
+
+    @FXML
+    private Pane rightPane1;
+
+    @FXML
+    private Button home;
+
+    @FXML
+    private Button stock;
+
+    @FXML
+    private Button orders;
+
+    @FXML
+    private Button payment;
 
 
     @FXML
@@ -99,6 +120,60 @@ public class Home implements Initializable {
             stage.getIcons().add(applicationIcon);
             stage.setScene(new Scene(changePwd));
             stage.show();
+
+        });
+        rightPane.translateXProperty().set(-1 * stackPane.getWidth());
+        var keyValue = new KeyValue(rightPane.translateXProperty(), 0, Interpolator.EASE_IN);
+        var keyFrame = new KeyFrame(Duration.millis(1000), keyValue);
+        var timeline = new Timeline(keyFrame);
+        timeline.setOnFinished(evt -> {
+            stackPane.getChildren().remove(rightPane1);
+        });
+        timeline.play();
+
+        stock.setOnMouseClicked(mouseEvent -> {
+            try {
+                Pane stockPane= FXMLLoader.load(getClass().getResource("/Views/StockPane.fxml"));
+                rightPane.getChildren().add(stockPane);
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+
+
+        });
+
+        orders.setOnMouseClicked(mouseEvent -> {
+            try {
+                Pane stockPane= FXMLLoader.load(getClass().getResource("/Views/OrderPane.fxml"));
+                rightPane.getChildren().add(stockPane);
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+
+
+        });
+        home.setOnMouseClicked(mouseEvent -> {
+            try
+            {
+                Pane stockPane=FXMLLoader.load(getClass().getResource("/Views/HomePane.fxml"));
+                rightPane.getChildren().add(stockPane);
+            }
+            catch (Exception ex){
+                ex.printStackTrace();
+            }
+        });
+
+        payment.setOnMouseClicked(mouseEvent -> {
+            try {
+                Pane stockPane= FXMLLoader.load(getClass().getResource("/Views/BillPane.fxml"));
+                rightPane.getChildren().add(stockPane);
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+
 
         });
 
