2ab76da565ef2dd831071ee866970be5beecc3f7
dorontinaj <58146212+dorontinaj@users.noreply.github.com>
2020-06-28 17:38:18 +0200

    Update FaturaHelper.java

--- TEKH/src/Controllers/FaturaHelper.java
+++ TEKH/src/Controllers/FaturaHelper.java
@@ -1,1 +1,30 @@
+package Controllers;
 
+import java.util.Date;
+
+public class FaturatHelper {
+
+        private Integer id;
+        private String person;
+        private double totalCost;
+        private Date time;
+        private String product;
+        private double cost;
+
+
+
+    FaturatHelper(Integer id, String person, Date time, double totalCost){
+        this.id=id;
+        this.person=person;
+        this.time=time;
+        this.totalCost=totalCost;
+    }
+
+    FaturatHelper(Integer id, String product, double cost){
+        this.id=id;
+        this.product=product;
+        this.cost=cost;
+    }
+
+
+}
