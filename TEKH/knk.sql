1b8620e872c27f1d06ea3b15ef32935f02025940
Enis Berisha <58141985+enisbe1@users.noreply.github.com>
2020-06-28 23:39:34 +0200

    Update knk.sql

--- TEKH/knk.sql
+++ TEKH/knk.sql
@@ -8,8 +8,8 @@ surname varchar (40),
 roli varchar (30),
 data_fillimit datetime
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 select * from employees;
+select * from users;
 
 SET FOREIGN_KEY_CHECKS=ON;
 
@@ -18,8 +18,6 @@ username varchar (40) primary key,
 password varchar (30),
 roli varchar (30)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
-insert into users values ('enis.1','Berisha1','menaxher');
-select * from users;
 
 alter table employees modify data_fillimit date;
 
@@ -45,8 +43,12 @@ sasia int,
 cmimi double
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-delete from users where id=4;
 
+create table lastLogged(
+username varchar(50),
+kohakyqjes datetime)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+select * from lastLogged;
+select * from exEmployees;
 
 create table faturat(
 id int auto_increment primary key,
@@ -54,16 +56,28 @@ personi varchar(50),
 koha datetime,
 shuma double)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+create table signedOut(
+username varchar(50),
+kohadaljes datetime)Engine=InnoDB default Charset=utf8;
+select * from signedOut;
+
 create table mbrendafatures(
 id int,
 produkti varchar(30),
 cmimi double,
 foreign key(id) references faturat(id))ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table lastLogged(
-username varchar(50),
-kohakyqjes datetime)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+create table orders(
+name varchar(30),
+id integer ,
+koha datetime,
+price double,
+VAT double,
+total double,
+aprovuar varchar(25)
+);
+insert into orders values('fatura',3,'2020-03-20 23:19:18',82.1,18,82.1,'a');
+select * from orders;
 CREATE TABLE `products` (
   `name` varchar(40) DEFAULT NULL,
   `price` double DEFAULT NULL,
